99aa4858e993f8cf391502bdbad24d950f829f47
AA <AA>
2014-10-10 00:52:57 +0800

    add

---
+++ TextDateControl.js
@@ -0,0 +1,18 @@
+﻿
+
+window.onload = function () { TextDate.ResetStyle() };
+
+TextDate = {
+    ResetStyle: function () {
+        var vItems = document.getElementsByTagName('input');
+        if (!vItems) {
+            return;
+        }
+        for (var i = 0; i < vItems.length; i++) {
+            var vItem = vItems[i];
+            if (vItem.getAttribute('type') == 'date') {
+                alert(vItem);
+            }
+        }
+    }
+}
